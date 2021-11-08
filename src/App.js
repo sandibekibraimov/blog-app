0a5ef5a43159372bfdf3ee4ea59efaf44580f5ab
sandibekibraimov <sandibek@gmail.com>
2021-11-07 23:24:30 -0500

    write page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import TopBar from './components/topbar/TopBar';
 import HomePage from './pages/home/HomePage';
 import PostPage from './pages/post/PostPage';
+import WritePage from './pages/write/WritePage';
 
 const App = () => {
   return (
     <Fragment>
       <TopBar />
       {/* <HomePage /> */}
-      <PostPage />
+      {/* <PostPage /> */}
+      <WritePage />
     </Fragment>
   );
 };
--- src/pages/write/WritePage.jsx
+++ src/pages/write/WritePage.jsx
@@ -2,7 +2,38 @@ import React from 'react';
 import './writepage.css';
 
 const WritePage = () => {
-  return <div className='writepage'></div>;
+  return (
+    <div className='write'>
+      <img
+        src='https://images.pexels.com/photos/7753054/pexels-photo-7753054.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        alt=''
+        className='writeImg'
+      />
+      <form className='writeForm'>
+        <div className='writeFormGroup'>
+          <label htmlFor='fileInput'>
+            <i className='writeIcon fas fa-plus'></i>
+          </label>
+          <input type='file' id='fileInput' style={{ display: 'none' }} />
+          <input
+            type='text'
+            placeholder='Title'
+            className='writeInput'
+            autoFocus
+          />
+        </div>
+
+        <div className='writeFormGroup'>
+          <textarea
+            placeholder='Tell your story'
+            type='text'
+            className='writeInput writeText'
+          ></textarea>
+        </div>
+        <button className='writeSubmit'>Publish</button>
+      </form>
+    </div>
+  );
 };
 
 export default WritePage;
