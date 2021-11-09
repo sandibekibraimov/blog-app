b83e3aa78390a5c3cf87e26cc9188b5428038964
sandibekibraimov <sandibek@gmail.com>
2021-11-08 22:51:38 -0500

    added file upload

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
 const dbConnect = require('./config/dbConnect');
@@ -12,6 +13,21 @@ dbConnect();
 
 app.get('/', (req, res) => {
   res.send('api is working');
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, callBackFn) => {
+    callBackFn(null, 'assets');
+  },
+  filename: (req, file, callBackFn) => {
+    callBackFn(null, 'image.png');
+  },
+});
+
+const upload = multer({ storage });
+
+app.use('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json({ msg: 'File has been uploaded' });
 });
 
 app.use('/api/auth', authRoutes);
