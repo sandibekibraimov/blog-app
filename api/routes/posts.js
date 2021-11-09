29a0c57f50377d13925a0c8dce76d3f20a43261d
sandibekibraimov <sandibek@gmail.com>
2021-11-08 21:51:49 -0500

    added delete post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -48,16 +48,22 @@ router.put('/:id', async (req, res) => {
 // delete post route
 
 router.delete('/:id', async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json({ msg: 'Successfully deleted' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json({ msg: 'Post had been deleted' });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json({ msg: 'You can delete only your post' });
     }
-  } else {
-    res.status(401).json({ msg: 'You may delete only your account' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 
